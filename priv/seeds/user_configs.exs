82faa303129474868da24ebef482533cbfaf399a
isaias@latitude <isaias.dm.tech@gmail.com>
2026-02-10 17:23:28 +0000

    added 1 seed

--- priv/seeds/user_configs.exs
+++ priv/seeds/user_configs.exs
@@ -1,3 +1,3 @@
 [
-  %{chat_id: nil, url_sources: []}
+  %{chat_id: "8224378910", url_sources: ["https://www.youtube.com/@Fireship", "https://www.youtube.com/@antirez", "https://www.youtube.com/@esadecimale", "https://www.youtube.com/@Tsoding", "https://www.youtube.com/@SavvyNik"]}
 ]
